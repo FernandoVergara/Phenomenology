cda4751d9a5068fa710254055f806fc08ca82178
asegura4488 <asegura4488@gmail.com>
2019-05-10 13:45:08 -0500

    reparacion

--- Analyzer/Analyzer/change.sh
+++
@@ -1,8 +0,0 @@
-typeset -i i=0
-for x in `cat names.dat` ; do
-mv $x SingleMuPt10_pythia8_cfi_py_GEN_SIM_DIGI_$i.root
-echo $x
-echo $i
-i=i+1
-done
-
--- Analyzer/Analyzer/output_folder/copy_hadro.sh
+++
@@ -1,5 +0,0 @@
-cp monotop1jet_left/monotop1jet_left_hadro_60-350.root ~/PhysicsPlotter_pheno/data/monotop1jet_left.root 
-cp monotop1jet_right/monotop1jet_right_hadro_60-350.root ~/PhysicsPlotter_pheno/data/monotop1jet_right.root 
-cp monotop1jet_full/monotop1jet_full_hadro_60-350.root ~/PhysicsPlotter_pheno/data/monotop1jet_full.root
-cp DY+jets/DY+jets_hadro_60-350.root ~/PhysicsPlotter_pheno/data/DY+jets.root 
-cp singletop/singletop_hadro_60-350.root ~/PhysicsPlotter_pheno/data/singletop.root 
